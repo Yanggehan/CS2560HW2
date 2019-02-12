7b927dcf4428a10a03c7577ede192c2f02a06c39
Yanggehan <33634849+Yanggehan@users.noreply.github.com>
2019-02-12 15:41:45 -0800

    task2

---
+++ hw2task2/task2.c
@@ -0,0 +1,17 @@
+/*
+ * task2.c
+ *
+ *  Created on: 2019年2月9日
+ *      Author: 111111
+ */
+
+
+#include <stdio.h>
+int main(void){
+	double oceanLevel = 1.5;
+	printf("now the oceanLevel is  is %lf\n",oceanLevel);
+	printf("the ocean’s level will be in 5 years is %lf\n",oceanLevel * 5);
+	printf("the ocean’s level will be in 7 years is %lf\n",oceanLevel * 7);
+	printf("the ocean’s level will be in 10 years is %lf\n",oceanLevel * 10);
+	return 0;
+}
